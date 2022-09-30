f9805d8bf33856d7567d695ca772ccebd6cb150f
Esteban, Kelly Brend <78207376+Esteban-KellyBrend@users.noreply.github.com>
2022-09-30 22:09:29 +0800

    Add files via upload

---
+++ Exercise 1.php
@@ -0,0 +1,23 @@
+<html>
+<head><title> Exercise 1 </title></head>
+<body>
+
+<?php
+$Fullname = "Kelly Brend A. Esteban";  
+echo $Fullname;
+echo "<br>";
+$College_Program= "BS CpE";  
+echo $College_Program;
+echo "<br>";
+$year_and_section = "3rd Year - 58043";  
+echo $year_and_section; 
+echo "<br>";
+$string = "Today, I will learn PHP programming";
+echo $string;
+
+?>
+
+</body>
+</html>
+
+
