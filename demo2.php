e028f604030261f8645d27ee666f8a86b396b777
Esteban, Kelly Brend <78207376+Esteban-KellyBrend@users.noreply.github.com>
2022-10-07 10:02:29 +0800

    Add files via upload

---
+++ demo2.php
@@ -0,0 +1,62 @@
+<html>
+<head><title>My Web Page</title></head>
+<?php
+
+$content = "Welcome to PHP Programming";
+$a = 5;
+$_msg = 'Hello, again';
+$x = 8;
+$y = 8;
+
+echo $content;
+echo "<br>".$a;
+echo "<br>";
+echo $_msg;
+//Arithmetic operations
+echo "<br>";
+echo $x+$y; 
+echo "<br>";
+echo $x-$y; 
+echo "<br>";
+echo $x*$y;
+echo "<br>";
+echo $x/$y; 
+echo "<br>". $x%$y;
+echo "<br>";
+echo $x**$y;
+//Comparison operations
+echo "<br>";
+if ($x==$y)
+{
+	echo "They are identical";
+}
+else
+{
+	echo "They are not identical";
+}
+//Increment/decrement operators
+//for loop
+/*for ($a = 5;$a>=1; $a--)
+{
+	echo "<br>".$a;
+}
+echo"<br>";*/
+//while loop
+$a = 5; 
+while($a>=1)
+{
+	echo "The number is: $a \n";
+	echo"<br>";
+	$a-=1;  //$a=$a-1
+}
+
+//do while loop
+$b = 5;
+	do
+	{
+		echo "<br>The number is:".$b;
+		$b--;
+	}
+	while ($b >= 1);
+?>
+</html>
