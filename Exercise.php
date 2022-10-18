19962a6b3514b37b54ad9ab8675861f10ba51c43
Esteban, Kelly Brend <78207376+Esteban-KellyBrend@users.noreply.github.com>
2022-10-18 19:34:06 +0800

    Add files via upload

---
+++ Exercise.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+<title>Exercise</title>
+<body align = center><h1>
+<?php
+
+$a = 58;
+
+if ($a>=70)
+{
+	$grade = "Passed";
+}
+else if($a>=60 && $a<70)
+{
+	$grade = "Remedial";
+}
+else
+{
+	$grade = "Failed";
+}
+echo "Grade: $a";
+echo "<br>";
+echo "Result: $grade";
+
+?></h1>
+</body>
+
+</head>
+
+</html>
